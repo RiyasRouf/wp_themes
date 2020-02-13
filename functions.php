37e67f1df95e20a987dfcdf7a40070340b7247a3
Riyas Rouf <31923379+roufriyas@users.noreply.github.com>
2020-02-13 11:59:00 +0400

    Delete functions.php

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-// codes
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-
-function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
